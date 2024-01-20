57b504ccb1d30b120c53aa16398b29514d4a4535
Samarth Vengurlekar <112164785+SamarthVengurlekar@users.noreply.github.com>
2024-01-20 13:29:19 +0530

    Add files via upload

---
+++ Assembly_Line_Control_GUI/AssemblyLineControl/OrderDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLineControl
+{
+    internal class OrderDetails
+    {
+        public string amount { get; set; }
+        public string customerID { get; set; }
+        public string date { get; set; }
+        public string delAddress { get; set; }
+        public string dueDate { get; set; }
+        public string orderID { get; set; }
+        public Dictionary<string,int> orderList { get; set; }
+        public string orderStatus { get; set; }
+        public string paymentType { get; set; }
+        public string payment_Status { get; set; }
+    }
+}
---
+++ Assembly_Line_Control_GUI/AssemblyLineControl/WarehouseLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLineControl
+{
+    public class WarehouseLocation
+    {
+        public string storageLocation;
+        public string path;
+        public string status;
+        public string productId;
+        public string typeId;
+    }
+}
